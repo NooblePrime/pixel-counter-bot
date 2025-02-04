1c8a4622b2cb5bfc3c417ee4efbe4539b2ba9186
Nooble <96099842+NooblePrime@users.noreply.github.com>
2025-02-03 18:37:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import praw
 from pprint import pprint
 import threading
 import random
+import requests
+import re
 from time import sleep
-from prawcore.exceptions import Forbidden
-from prawcore.exceptions import ServerError
+from prawcore.exceptions import Forbidden, ServerError
+from bs4 import BeautifulSoup
+from PIL import Image
+from io import BytesIO
 
 reddit = praw.Reddit(
     client_id="",
     client_secret="",
     password="",
-    user_agent="a bot made to count the pixels on a post",
+    user_agent="",
     username="pixel-counter-bot",
 )
 
@@ -58,126 +62,85 @@ def constructComment(submission, from_mention):
     if hasattr(submission, 'post_hint'):
         hint = submission.post_hint
         if hint == 'image' or hint == 'hosted:video':
-                
             source = vars(submission)['preview']['images'][0]['source']
             x = source['width']
             y = source['height']
-            comment_string += "The "
-            if hint == 'hosted:video':
-                comment_string += "video"
-            else:
-                comment_string += "image"
             size = x * y
             if size > 1 and random.randint(1, 50) == 50:
                 eat = True
                 size -= 1
             else:
                 eat = False
-            if spoiler:
-                comment_string += f" in this **POST** has >!{addComma(size)}({addComma(x)}×{addComma(y)})!< pixel"
-            else:
-                comment_string += f" in this **POST** has {addComma(size)}({addComma(x)}×{addComma(y)}) pixel"
-                
-            if size > 1:
-                comment_string += "s"
+            comment_string = re.sub(r'\{hint\}', 'video' if hint == 'hosted:video' else 'image', "The {hint} in this post has ")
+            comment_string += f"{'>!' if spoiler else ''}{addComma(size)}({addComma(x)}×{addComma(y)}){'!<' if spoiler else ''} pixel{'s' if size > 1 else ''}{'!' if hint != 'hosted:video' else ' per frame'}"
             if hint == 'hosted:video':
-
-                eat = False
-                frame_count = vars(submission)['media']['reddit_video']['duration']*30
-                if spoiler:
-                    comment_string += f" per frame and an estimated >!{(frame_count)}!< frames"
-                else:
-                    comment_string += f" per frame and an estimated {(frame_count)} frames"
-            comment_string += "!"
-            if hint == 'hosted:video':
-                if spoiler:
-                    comment_string += f"\n\nTotal pixels in video: >!{addComma(size*frame_count)}!<"
-                else:
-                    comment_string += f"\n\nTotal pixels in video: {addComma(size*frame_count)}"
+                frame_count = vars(submission)['media']['reddit_video']['duration'] * 30
+                comment_string += f" and {addComma(frame_count)} frame{'s' if frame_count > 1 else ''} for a total of {addComma(frame_count*size)} pixels!"
             if eat:
                 comment_string += "\n\nYou may have noticed that one pixel is missing from that calculation. That is because I stole it. That pixel is mine now, and you're not getting it back."
-            
     elif hasattr(submission, 'media_metadata'):
-            try:
-                image_list = vars(submission)["media_metadata"]
-                if len(image_list) > 1:
-                    pixel_total = 0
-                    comment_string += "This post contains multiple images!\n\n"
-                    i = 1
-                    try:
-                        media_ids = sortMedia(submission.gallery_data)
-                    except Exception as e:
-                        pprint(vars(submission))
-                        pprint(e)
-                        return "Oh no! Image sorting could not be completed because Reddit API failed to provide gallery attribute data."
-                    for item in media_ids:
-                        retry = True
-                        fails = 0
-                        while retry:
-                            if fails <= 6:
-                                try:
-                                    image_source = image_list[item]['s']
-                                    x = image_source['x']
-                                    y = image_source['y']
-                                    retry = False
-                                except Exception as e:
-                                    pprint(e)
-                                    print("Resolution fetch failed. Retrying...")
-                                    sleep(5)
-                                    fails += 1
-                            else:
-                                print("Resolution fetch failed too many times. Giving up on this post.")
-                                return "Whoops! It looks like I am unable to fetch the image data for this post because Reddit failed to process it."
-                        if spoiler:
-                            comment_string += f"Image {i} has >!{addComma(x * y)}({addComma(x)}×{addComma(y)})!< pixel"
+        try:
+            image_list = vars(submission)["media_metadata"]
+            if len(image_list) > 1:
+                pixel_total = 0
+                comment_string = "This post contains multiple images!\n\n"
+                try:
+                    media_ids = sortMedia(submission.gallery_data)
+                except Exception as e:
+                    pprint(vars(submission))
+                    pprint(e)
+                    return "Oh no! Image sorting could not be completed because Reddit API failed to provide gallery attribute data."
+                for i, item in enumerate(media_ids, 1):
+                    retry = True
+                    fails = 0
+                    while retry:
+                        if fails <= 6:
+                            try:
+                                image_source = image_list[item]['s']
+                                x = image_source['x']
+                                y = image_source['y']
+                                retry = False
+                            except Exception as e:
+                                pprint(e)
+                                print("Resolution fetch failed. Retrying...")
+                                sleep(5)
+                                fails += 1
                         else:
-                            comment_string += f"Image {i} has {addComma(x * y)}({addComma(x)}×{addComma(y)}) pixel"
-                        if x * y > 1:
-                            comment_string += "s"
-                        
-                        comment_string += ".\n\n"
-                        pixel_total += x * y
-                        i += 1
-                    comment_string += "Total pixels: " + addComma(pixel_total) + "."
-                else:
-                    for image in vars(submission)["media_metadata"]:
-                        retry = True
-                        fails = 0
-                        while retry:
-                            if fails <= 6:
-                                try:
-                                    image_source = vars(submission)['media_metadata'][image]['s']
-                                    x = image_source['x']
-                                    y = image_source['y']
-                                    size = x * y
-                                    if spoiler:
-                                        comment_string += f"This image has >!{addComma(size)}({addComma(x)}×{addComma(y)})!< pixel"
-                                    else:
-                                        comment_string += f"This image has {addComma(size)}({addComma(x)}×{addComma(y)}) pixel"
-
-                                    if size > 1:
-                                        comment_string += "s"
-                                    comment_string += "!"
-                                    retry = False
-                                except Exception as e:
-                                    pprint(e)
-                                    print("Resolution fetch failed. Retrying...")
-                                    sleep(5)
-                                    fails += 1
-                            else:
-                                print("Resolution fetch failed too many times. Giving up on this post.")
-                                return "Whoops! It looks like I am unable to fetch the image data for this post because Reddit failed to process it."
-            except TypeError as e:
-                print("Unexpected type error! Cannot process post!")
-                pprint(e)
-                return "Unexpected error! Error has been automatically reported to my developer."
+                            print("Resolution fetch failed too many times. Giving up on this post.")
+                            return "Whoops! It looks like I am unable to fetch the image data for this post because Reddit failed to process it."
+                    comment_string += f"Image {i} has {'>!' if spoiler else ''}{addComma(x * y)}({addComma(x)}×{addComma(y)}){'!<' if spoiler else ''} pixel{'s' if x * y > 1 else ''}.\n\n"
+                    pixel_total += x * y
+                comment_string += "Total pixels: " + addComma(pixel_total) + "."
+            else:
+                for image in vars(submission)["media_metadata"]:
+                    retry = True
+                    fails = 0
+                    while retry:
+                        if fails <= 6:
+                            try:
+                                image_source = vars(submission)['media_metadata'][image]['s']
+                                x = image_source['x']
+                                y = image_source['y']
+                                size = x * y
+                                comment_string += f"The image in this post has {'>!' if spoiler else ''}{addComma(size)}({addComma(x)}×{addComma(y)}){'!<' if spoiler else ''} pixel{'s' if size > 1 else ''}!"
+                                retry = False
+                            except Exception as e:
+                                pprint(e)
+                                print("Resolution fetch failed. Retrying...")
+                                sleep(5)
+                                fails += 1
+                        else:
+                            print("Resolution fetch failed too many times. Giving up on this post.")
+                            return "Whoops! It looks like I am unable to fetch the image data for this post because Reddit failed to process it."
+        except TypeError as e:
+            print("Unexpected type error! Cannot process post!")
+            pprint(e)
+            return "Unexpected error! Error has been automatically reported to my developer."
     elif hasattr(submission, 'crosspost_parent_list'):
-        comment_string += constructComment(praw.models.Submission(reddit = reddit, id = vars(submission)['crosspost_parent_list'][0]['id']), from_mention)
+        comment_string += constructComment(praw.models.Submission(reddit=reddit, id=vars(submission)['crosspost_parent_list'][0]['id']), from_mention)
     return comment_string
 
-
 def attemptComment(submission, item, from_mention):
-    
     comment_string = constructComment(submission, from_mention)
     if len(comment_string) > 0:
         try:
@@ -193,56 +156,84 @@ def attemptComment(submission, item, from_mention):
                     submission.reply(comment_string + bot_statement)
             print("Replied to a post!")
         except Forbidden:
-            print("Cannot comment on banned subreddit.")
+            try:
+                item.author.message(subject="I was banned.", message=f"It appears that I am banned from r/{submission.subreddit.display_name}, so I am unable to reply to [this comment]({submission.permalink}). The constructed reply is as follows:\n\n{comment_string}" + bot_statement)
+            except Forbidden:
+                pass
         except Exception as e:
             print("Unexpected error!")
             pprint(e)
     else:
         print("Cannot comment on this post!")
 
+def get_image_resolution(url):
+    try:
+        response = requests.get(url, stream=True)
+        response.raw.decode_content = True
+        image = Image.open(BytesIO(response.raw.read(1024)))  # Read only the first 1024 bytes
+        return image.size  # returns (width, height)
+    except Exception as e:
+        print(f"Failed to get image resolution for {url}: {e}")
+        return None
+
+def handleItem(submission, item, from_comment):
+    retry = True
+    while retry:
+        try:
+            if from_comment and item.parent_id.startswith("t1_"):
+                try:
+                    soup = BeautifulSoup(item.parent().body_html, features="html.parser")
+                    links = soup.find_all('a')['href']
+                    image = None
+                    for link in links:
+                        if re.findall(r'\.(?:png|jpg|jpeg|gif|webp)', link) != []:
+                            image = link.replace('preview', 'i', 1)
+                            break
+                    if image == None:
+                        raise TypeError
+                    resolution = get_image_resolution(link)
+                    if resolution:
+                        width, height = resolution
+                        try:
+                            item.reply(f"The image in this comment has {addComma(width * height)}({addComma(width)}×{addComma(height)}) pixel{'s' if (width*height) > 1 else ''}!" + bot_statement)
+                        except Forbidden:
+                            try:
+                                item.author.message(subject="I was banned.", message=f"It appears that I am banned from r/{item.subreddit.display_name}, so I am unable to reply to [this comment]({item.context}). The constructed reply is as follows:\n\nThe image in this comment has {addComma(width * height)}({addComma(width)}×{addComma(height)}) pixel{'s' if (width*height) > 1 else ''}!" + bot_statement)
+                            except Forbidden:
+                                pass
+                except TypeError:
+                    attemptComment(submission, item, from_comment)
+                item.mark_read()
+                print("Replied to a comment!")
+            else:
+                attemptComment(submission, item, from_comment)
+            retry = False
+        except ServerError:
+            print("Server error. Retrying...")
+            sleep(5)
+            retry = True
+
 def submissionStream():
     for submission in reddit.subreddit("countablepixels").stream.submissions():
-        retry = True
-        while retry:
-            try:
-                if not checkIfVisited(vars(submission)['id']):
-                    attemptComment(submission, None, False)
-                    logVisit(vars(submission)['id'])
-                retry = False
-            except ServerError:
-                sleep(5)
-                retry = True
+        if not checkIfVisited(vars(submission)['id']):
+            handleItem(submission, None, False)
+            logVisit(vars(submission)['id'])
 
 def mentionStream():
-    retry = True
-    while retry:
-        try:
-            for item in reddit.inbox.unread(limit=100):
-                info = vars(item)
-                if item.new and info['type'] == 'username_mention':
-                    attemptComment(item.submission, item, True)
-                    item.mark_read()
-            retry = False
-        except ServerError:
-            print("Server error. Retrying...")
-            sleep(5)
-            retry = True
-    
+    for item in reddit.inbox.unread(limit=100):
+        info = vars(item)
+        if item.new and info['type'] == 'username_mention':
+            handleItem(item.submission, item, True)
+            item.mark_read()
+
     print("Successfully checked old inbox items.")
 
-    retry = True
-    while retry:
-        try:
-            for item in reddit.inbox.stream():
-                info = vars(item)
-                if item.new and info['type'] == 'username_mention':
-                    attemptComment(item.submission, item, True)
-                    item.mark_read()
-            retry = False
-        except ServerError:
-            print("Server error. Retrying...")
-            sleep(5)
-            retry = True
+    for item in reddit.inbox.stream():
+        info = vars(item)
+        if item.new and info['type'] == 'username_mention':
+            handleItem(item.submission, item, True)
+            item.mark_read()
+
 
 print("Startup successful.")
 
